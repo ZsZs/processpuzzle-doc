4224b015598d7e7ec102cd52f3996a03d17763b8
Zsolt Zsuffa <zsolt.zsuffa@gmail.com>
2018-09-20 07:49:53 +0000

    intermediate status

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -18,10 +18,10 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('processpuzzle-doc');
   }));
-  it('should render title in a h1 tag', async(() => {
+  it('should render title in a h3 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to processpuzzle-doc!');
+    expect(compiled.querySelector('h3').textContent).toContain('Welcome to processpuzzle-doc!');
   }));
 });
