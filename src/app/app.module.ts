e63b798c3446addc775686ca36108eb42a969565
Zsolt Zsuffa <zsolt.zsuffa@hpe.com>
2018-10-16 11:41:01 +0200

    configured to use processpuzzle-util

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,39 @@
-// Angular
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-// Third party components
-import { MaterializeModule } from 'angular2-materialize';
-
-// ProcessPuzzle components
 import { AppComponent } from './app.component';
-import { ProcesspuzzleDocModule } from 'processpuzzle-doc';
+import { HomeComponent } from './home.component';
+import { environment } from '../environments/environment';
+import { ProcesspuzzleUtilLibModule } from 'processpuzzle-util';
+import { RemoteApiConfiguration } from 'processpuzzle-util';
+import { PageNotFoundComponent } from './page-not-found.component';
+import { SmartDocumentComponent } from './smart-document/smart-document.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'smart-document', component: SmartDocumentComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
+
+const apiConfiguration: RemoteApiConfiguration = {
+  protocol: environment['contactService'].protocol,
+  host: environment['contactService'].host,
+  contextPath: environment['contactService'].contextPath,
+  resourcePath: environment['contactService'].resourcePath,
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    SmartDocumentComponent
   ],
   imports: [
     BrowserModule,
-    MaterializeModule,
-    ProcesspuzzleDocModule
+    ProcesspuzzleUtilLibModule.forRoot( apiConfiguration ),
+    RouterModule.forRoot( appRoutes, { enableTracing: true } )
   ],
   providers: [],
   bootstrap: [AppComponent]
